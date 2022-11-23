7aa72fabf4dffc61c8f6b3a638a7acd256afa659
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-11-22 22:37:15 -0700

    completed unit 1 need to add videos

---
+++ starter/graphs.py
@@ -0,0 +1,79 @@
+############################# TO DO #####################################
+
+# write import statement for psycopg2
+import psycopg2
+
+# write import statement to import Error from psycopg2
+from psycopg2 import Error
+
+# import the get_connected function from your database.py file
+from database import get_connected
+
+# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+
+try:
+    # connection to database
+    connection = get_connected()
+
+    # creates cursor
+    cursor = connection.cursor()
+
+############################# TO DO #####################################
+
+##### create product_revenue_query with the query from parts 3 and 4 which get the total revenue from each product category
+    product_revenue_query ="""SELECT SUM(unit_price), product_category FROM invoices 
+            GROUP BY product_category
+            ORDER BY SUM DESC"""
+
+#### follow the project directions to create a function called get_revenue which fetches the results from the product_revenue_query
+    def get_revenue ():
+        with connection:
+            with cursor:
+                cursor.execute(product_revenue_query)
+                return cursor.fetchall()
+
+
+#### create a variable called product_revenue and set it equal to the get_revenue function invoked 
+    product_revenue=get_revenue()
+#### print the product_revenue variable to look at the structure of the variable to help you write the next function
+    #----------------------PRINT--------------#  
+    #print(product_revenue)  #successfully fetched a list
+    
+#### write a function which loops through the product_revenue variable and creates two lists - one with product categories and one with total revenue values
+    def generate_lists (input):
+        revenue_list=[]
+        product_list=[]
+        for x in input:
+            revenue_list.append(x[0])
+            product_list.append(x[1])
+        return (revenue_list, product_list)  #returns a tuple of lists  
+    input = generate_lists(product_revenue) #lists are returned as a tuple of arrays.     
+#### follow the project directions to create a function which makes a bar chart in matplotlib using the total revenue from each product category
+    def create_bar_chart():
+        figure = plt.figure()
+        data=input[0]
+        labels=input[1]
+        plt.xticks(range(len(labels)),labels,rotation=70)
+        plt.xlabel("Product Category")
+        plt.ylabel("Revenue(USD)")
+        plt.title("Product Revenue by Category")
+        plt.bar(labels,data)
+        return figure
+    graph = create_bar_chart(input)
+    
+    plt.show()
+
+#### invoke the create_bar_chart function
+
+#### use plt.show() to show a pop-up of the bar chart you created
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL DB", error)
+
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("DB connection is closed.")
